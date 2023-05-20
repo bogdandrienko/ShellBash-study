f6c32dcbdbc81227120ee5a7e00e994b2269f0e5
Bogdan <67187037+bogdandrienko@users.noreply.github.com>
2023-05-20 12:13:06 +0600

    update

--- __postgresql.py
+++ __postgresql.py
@@ -1,58 +1,56 @@
 
 def postgresql_ubuntu():
     """
-    # todo postgresql actions
+    # TODO postgresql actions
     sudo systemctl status postgresql
     sudo systemctl stop postgresql
     sudo systemctl restart postgresql
-    # todo postgresql actions
 
-    # create password for postgres
+    # TODO create password for postgres
     sudo passwd postgres
 
-    # login to postgres user
+    # TODO login to postgres user
     sudo -i -u postgres
 
-    # logout to postgres user
+    # TODO logout to postgres user
     exit
 
-    # enter to SQL database
+    # TODO enter to SQL database
     psql
     psql postgres
 
-    # database list
+    # TODO database list
     \l
 
-    # table list
+    # TODO table list
     \d
 
-    # unknown
+    # TODO connect to database
     \c pg_db
     \connect pg_db
 
-    # quit from database
+    # TODO quit from database
     \q
     \quit
 
-    # todo create new user and database
-    # postgres-user
+    # TODO create new user and database
     createuser pgs_usr
     createdb pgs_db -O pgs_usr
 
-    # postgres-psql
+    # TODO postgres-psql
     CREATE USER pgs_usr WITH PASSWORD '12345Qwerty!';
     CREATE DATABASE pgs_db OWNER pgs_usr;
 
-    # change password
+    # TODO change password
     alter user pgs_usr with password '12345Qwerty!';
 
-    # grant role
+    # TODO grant role
     GRANT ALL PRIVILEGES ON DATABASE pgs_db TO pgs_usr;
     GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public to pgs_usr;
     GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public to pgs_usr;
     GRANT ALL PRIVILEGES ON ALL FUNCTIONS IN SCHEMA public to pgs_usr;
 
-    # todo check 1
+    # TODO check 1
     CREATE TABLE zarplata ( id serial PRIMARY KEY, username VARCHAR ( 50 ) UNIQUE NOT NULL, salary INT );
 
     select * from zarplata;
@@ -60,9 +58,8 @@ def postgresql_ubuntu():
     insert into zarplata (username, salary) VALUES ('Bogdan', '60000'), ('Alice', '80000');
 
     delete from zarplata where username = 'Bogdan';
-    # todo check 1
 
-    # todo check 2
+    # TODO check 2
     create table users_new (id serial not null unique, username varchar(255) not null, date_register timestamp not null default now(), active_status bool default 'false');
     alter table users_new drop column registered_at;
     alter table users_new add column registered_at timestamp not null default now();
@@ -72,27 +69,26 @@ def postgresql_ubuntu():
     select * from users_new;
     \q
     exit
-    # todo check 2
 
-    # todo check 3
+    # TODO check 3
     create table books (id serial not null unique, title varchar(255) not null, author varchar(255) not null);
     \q
     exit
-    # todo check 3
 
-    # todo share data on net
+    # TODO share data on net
     sudo nano /etc/postgresql/14/main/postgresql.conf
-    <file> /etc/postgresql/14/main/postgresql.conf
+
+    <TODO file> /etc/postgresql/14/main/postgresql.conf
     listen_addresses = '*'
-    <file> /etc/postgresql/14/main/postgresql.conf
+    </TODO file> /etc/postgresql/14/main/postgresql.conf
 
     sudo nano /etc/postgresql/14/main/pg_hba.conf
-    <file> /etc/postgresql/14/main/pg_hba.conf
+
+    <TODO file> /etc/postgresql/14/main/pg_hba.conf
     host    all             all             192.168.0.165/32         scram-sha-256
     host    all             all             all         scram-sha-256
     host    all             all             all         trust (!danger)
-    <file> /etc/postgresql/14/main/pg_hba.conf
-    # todo share data on net
+    </TODO file> /etc/postgresql/14/main/pg_hba.conf
 
     """
     pass
